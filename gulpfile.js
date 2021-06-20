0974982a278a3af1d27196d9f51e9679f2742f5f
Vlad Zusko <vladzvu11@gmail.com>
2021-06-20 13:27:00 +0300

    Added basic setup

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+
+const defaults = {
+    htmlSource: './',
+    sassSource: './sass/',
+    cssSource: './css/',
+};
+
+gulp.task('sass', () => {
+    return gulp
+        .src(defaults.sassSource + '*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(
+            autoprefixer(
+                'last 2 version',
+                'safari 10',
+                'ie 10',
+                'opera 12.1',
+                'ios 9',
+                'android 4',
+            ),
+        )
+        .pipe(gulp.dest(defaults.cssSource))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(defaults.cssSource))
+        .pipe(browserSync.stream());
+});
+
+gulp.task(
+    'serve',
+    gulp.parallel('sass', () => {
+        browserSync.init({
+            server: {
+                baseDir: defaults.htmlSource,
+            },
+        });
+    }),
+);
+
+gulp.task('reload', () => {
+    browserSync.reload();
+});
+
+gulp.task(
+    'watch',
+    gulp.parallel('serve', () => {
+        gulp.watch(defaults.sassSource + '*.sass').on(
+            'change',
+            gulp.parallel('sass', 'reload'),
+        );
+        gulp.watch(defaults.htmlSource + '*.html').on(
+            'change',
+            gulp.parallel('reload'),
+        );
+    }),
+);
